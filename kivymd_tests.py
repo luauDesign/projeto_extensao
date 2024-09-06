98a44db525a2e8db5de0716783fcdbb1a7114304
luauDesign <luau.design.df@gmail.com>
2024-09-06 14:02:51 -0300

    muda para 4 telas, dividindo lista de alunos e eventos

--- kivymd_tests.py
+++ kivymd_tests.py
@@ -5,174 +5,75 @@ kivy e kivymd tests
 @author: Luis
 """
 
-# principais
-from kivymd.app import MDApp
-from kivy.lang import Builder
-
-# containers
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.uix.screenmanager import Screen
-from kivymd.uix.screen import MDScreen
-
-# elementos
-from kivymd.uix.label import MDLabel
-from kivymd.uix.datatables import MDDataTable
-from kivymd.uix.button import MDFillRoundFlatButton
-from kivymd.uix.card import MDCard
-
-# helpers
 from kivy.metrics import dp
 
+from kivymd.app import MDApp
+from kivymd.uix.datatables import MDDataTable
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.button import MDRaisedButton
 
-# KV file string for defining screens
-KV = '''
-ScreenManager:
-    MainScreen:
-    ProjectScreen:
-    SubjectScreen:
-    EventScreen:
 
-<MainScreen>:
-    name: 'mainscreen'
+class Example(MDApp):
+    data_tables = None
 
-<ProjectScreen>:
-    name: 'projectscreen'
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
 
-<SubjectScreen>:
-    name: 'subjectscreen'
+        layout = MDFloatLayout()  # root layout
+        # Creating control buttons.
+        button_box = MDBoxLayout(
+            pos_hint={"center_x": 0.5},
+            adaptive_size=True,
+            padding="24dp",
+            spacing="24dp",
+        )
 
-<EventScreen>:
-    name: 'eventscreen'
-'''
+        for button_text in ["Add row", "Remove row"]:
+            button_box.add_widget(
+                MDRaisedButton(
+                    text=button_text, on_release=self.on_button_press
+                )
+            )
 
-#%% tela principal
-
-# Main Screen Class
-class MainScreen(Screen):
-    initialized = False
-
-    def on_enter(self, *args):
-        if self.initialized:
-            pass
-        
-        #constroi a tela
-        layout = MDBoxLayout(orientation='vertical')
-        self.add_widget(layout)
-        
-        #card
-        
-        
-        #titulo
-        label = MDLabel(text="Main Screen", halign="center")
-        layout.add_widget(label)
-        
-        #tabela de projetos
-        
-        
-        #barra inferior
-        
-        
-        #para nao adicionar de novo
-        self.initialized = True
-
-#%% tela do projeto
-
-# Project Screen Class
-class ProjectScreen(Screen):
-    initialized = False
-    
-    def on_enter(self, *args):
-        if self.initialized:
-            pass
-        
-        #controi a tela
-        layout = MDBoxLayout(orientation='vertical')
-        self.add_widget(layout)
-        
-        #titulo
-        label = MDLabel(text="Project Screen", halign="center")
-        layout.add_widget(label)
-
-        #tabela
-        data_table = MDDataTable(
+        # Create a table.
+        self.data_tables = MDDataTable(
+            pos_hint={"center_y": 0.5, "center_x": 0.5},
             size_hint=(0.9, 0.6),
-            pos_hint={'center_x': 0.5, 'center_y': 0.5},
-            use_pagination=True,
+            use_pagination=False,
             column_data=[
                 ("No.", dp(30)),
-                ("Name", dp(30)),
-                ("Age", dp(30)),
+                ("Column 1", dp(40)),
+                ("Column 2", dp(40)),
+                ("Column 3", dp(40)),
             ],
-            row_data=[
-                ("1", "John Doe", "29"),
-                ("2", "Jane Doe", "32"),
-                ("3", "Sam Smith", "45"),
-            ],
+            row_data=[("1", "1", "2", "3")],
         )
-        layout.add_widget(data_table)
-        
-        #barra inferior
-        
-        #para nao adicionar de novo
-        self.initialized = True
+        # Adding a table and buttons to the toot layout.
+        layout.add_widget(self.data_tables)
+        layout.add_widget(button_box)
 
-#%% tela de alunos/pessoas
+        return layout
 
-# Subject Screen Class
-class SubjectScreen(Screen):
-    initialized = False
-    
-    def on_enter(self, *args):
-        if self.initialized:
+    def on_button_press(self, instance_button: MDRaisedButton) -> None:
+        '''Called when a control button is clicked.'''
+
+        try:
+            {
+                "Add row": self.add_row,
+                "Remove row": self.remove_row,
+            }[instance_button.text]()
+        except KeyError:
             pass
-        
-        self.clear_widgets()
-        layout = MDBoxLayout(orientation='vertical')
-        self.add_widget(layout)
 
-        label = MDLabel(text="Subject Screen", halign="center")
-        layout.add_widget(label)
-        
-        #para nao adicionar de novo
-        self.initialized = True
+    def add_row(self) -> None:
+        last_num_row = int(self.data_tables.row_data[-1][0])
+        self.data_tables.add_row((str(last_num_row + 1), "1", "2", "3"))
 
-#%% tela de aulas/eventos
-
-# Event Screen Class
-class EventScreen(Screen):
-    initialized = False
-    
-    def on_enter(self, *args):
-        if self.initialized:
-            pass
-        
-        self.clear_widgets()
-        layout = MDBoxLayout(orientation='vertical')
-        self.add_widget(layout)
-
-        label = MDLabel(text="Event Screen", halign="center")
-        layout.add_widget(label)
-        
-        #para nao adicionar de novo
-        self.initialized = True
-
-#%% app
-
-# Main App Class
-class ProjetoSocial(MDApp):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        
-        # Config
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Teal"
-        
-    def build(self):
-        # Load the main KV layout string and return it
-        return Builder.load_string(KV)
+    def remove_row(self) -> None:
+        if len(self.data_tables.row_data) > 1:
+            self.data_tables.remove_row(self.data_tables.row_data[-1])
 
 
-# Run the app
-if __name__ == '__main__':
-    app = ProjetoSocial()
-    app.run()
+Example().run()
--- projeto_social.py
+++ projeto_social.py
@@ -1,121 +1,358 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Aug  9 15:56:10 2024
-App para facitar o controle de projetos sociais quanto a
-alunos/pessoas atendidas e aulas/eventos realizados
-@author: Luis Guimaraes
+app para facilitar o trabalho de acompanhar alunos e eventos em
+projetos sociais voltados a arte ou esportes
+@author: Luis
 """
 
-# principais
+#outros
+import pandas as pd
+
+#principais
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.core.window import Window
 
-# containers
+#containers
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 
-# elementos
+#elementos
 from kivymd.uix.label import MDLabel
 from kivymd.uix.datatables import MDDataTable
-from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.datatables.datatables import CellRow
+from kivymd.uix.button import MDFillRoundFlatButton, MDIconButton
 from kivymd.uix.card import MDCard
+from kivy.uix.image import Image
 
-# helpers
-from kivy.metrics import dp
+#helpers
+from kivy.metrics import dp, mm, Metrics
 
-
-# KV file string for defining screens
+#define as telas pela kvlang #TODO: arrumar a ordem quando a navegação estiver pronta
 KV = '''
-ScreenManager:
+MDScreenManager:
     MainScreen:
-    ProjectScreen:
+    ProjectAlunosScreen:
+    ProjectEventosScreen:
     SubjectScreen:
     EventScreen:
 
 <MainScreen>:
-    name: 'mainscreen'
+    name: 'MainScreen'
 
-<ProjectScreen>:
-    name: 'projectscreen'
+<ProjectAlunosScreen>:
+    name: 'ProjectAlunosScreen'
+
+<ProjectEventosScreen>:
+    name: 'ProjectEventosScreen'
 
 <SubjectScreen>:
-    name: 'subjectscreen'
+    name: 'SubjectScreen'
 
 <EventScreen>:
-    name: 'eventscreen'
+    name: 'EventScreen'
 '''
+
+#%% helpers
+
+'''
+estrutura da tabela:
+--------------------    
+kivymd.uix.datatables.datatables.MDDataTable
+    0: kivy.factory.TableContainer
+        0: kivymd.uix.datatables.datatables.TableData
+            0: kivymd.uix.datatables.datatables.TableRecycleGridLayout
+                [i: kivymd.uix.datatables.datatables.CellRow]
+                    0: kivymd.uix.card.card.MDSeparator
+                    1: kivymd.uix.boxlayout.MDBoxLayout
+                        0: kivymd.uix.boxlayout.MDBoxLayout
+                            0: kivymd.uix.label.label.MDLabel
+                            0: kivymd.uix.label.label.MDIcon
+                                0: kivymd.uix.label.label.MDLabel
+                        1: kivymd.uix.selectioncontrol.selectioncontrol.MDCheckbox
+                            0: kivymd.uix.label.label.MDLabel
+'''
+
+#TODO: fazer isso funcionar
+def adjust_row_heights(data_table, *args):
+    #aparentemente, só as rows na tela e proximas da tale 'existem'
+    print(len(data_table.children[0].children[0].children[0].children), ' rows')
+    
+    for row in data_table.children[0].children[0].children[0].children:
+        print(type(row))
+        row.adaptive_height = False
+        row.minimum_height = mm(10)
+        row.size_hint_y = None
+        row.height = mm(10)
+        row.padding = (0, 0)
+        
+        label = row.children[1].children[0]
+        print(type(label))
+        label.adaptive_height = False
+        label.size_hint_y = None
+        label.minimum_height = mm(10)
+        label.height = mm(10)
+        label.padding = (0, 0)
 
 #%% tela principal
 
 # Main Screen Class
 class MainScreen(Screen):
     initialized = False
+    data_table = None
 
     def on_enter(self, *args):
         if self.initialized:
             pass
         
+        #TODO: alternativamente, testar se isso não é muito lento ou cria lixo na memória:
+        #todos os widgets serão descartados e recriados, mas isso pode simplificar algumas coisas
+        #as outras telas provavelmente precisarao ser assim porque o conteudo das tabelas muda
+        #self.clear_widgets()
+        
         #constroi a tela
-        layout = MDBoxLayout(orientation='vertical')
+        layout = MDBoxLayout(orientation='vertical',
+                             md_bg_color='teal')
         self.add_widget(layout)
         
-        #card
+        # #imagem
+        # card = Image(source='mainbg.jpg', 
+        #              size_hint=(1, .25),
+        #              pos_hint={"top": 1, "center_x": .5})
+        # layout.add_widget(card)
         
+        #cabeçalho
+        line0 = MDBoxLayout(orientation='horizontal',
+                            size_hint=(1, None),
+                            minimum_height=0)
+        layout.add_widget(line0)
         
         #titulo
-        label = MDLabel(text="Main Screen", halign="center")
-        layout.add_widget(label)
+        label = MDLabel(text='Projetos',
+                        theme_text_color='Custom',
+                        font_style='H6',
+                        text_color='white',
+                        halign='center',
+                        valign='top',
+                        size_hint=(1, None)
+                        )
+        label.bind(texture_size=lambda instance, value: instance.setter('height')(instance, instance.texture_size[1] + 30))
+        line0.add_widget(label)
         
-        #tabela de projetos
+        #tabela #TODO: carregar se ja existir, ou criar se nao existir
+        data_table = MDDataTable(size_hint=(1, .9), padding=5, elevation=2, 
+                                 pos_hint={'center_x': 0.5, 'center_y': 0.5},
+                                 use_pagination=False, rows_num=9999,
+                                 #colunas
+                                 column_data=[('nome', self.width),(f'', dp(0))],
+                                 #linhas
+                                 row_data=[(app.projetos[x]['nome'], f'') for x in range(len(app.projetos))]
+                                 )
+        data_table.ids.container.remove_widget(data_table.header)
+        data_table.sorted_on = "ID"
+        data_table.sorted_order = "ASC" #"DSC"
+        data_table.bind(on_row_press=self.on_row_press)
+        self.data_table = data_table
+        layout.add_widget(data_table)
         
-        
-        #barra inferior
-        
+        #TODO: barra inferior
         
         #para nao adicionar de novo
         self.initialized = True
+    
+    def on_row_press(self, instance_table, instance_row):
+        print('on_row_press')
+        
+        global itable, irow
+        itable, irow = instance_table, instance_row
+        
+        #indentify which project the row belongs to
+        #can't use the parent.children index because it loads and unloads rows dynamically
+        for i in range(len(app.projetos)):
+            #find the project
+            if app.projetos[i]['nome']==irow.text:
+                #set it as the current project
+                app.curproject = app.projetos[i]
+                #config the transition if not already configured
+                if type(app.root.transition) != FadeTransition:
+                    app.root.transition = FadeTransition()
+                #go to the project:alunos screen
+                app.root.current = 'ProjectAlunosScreen'
+
+#para facilitar debuggar:
+itable : MDDataTable = None
+irow : CellRow = None
 
 #%% tela do projeto
 
-# Project Screen Class
-class ProjectScreen(Screen):
+class ProjectAlunosScreen(Screen):
     initialized = False
-    
-    def on_enter(self, *args):
-        if self.initialized:
-            pass
+    data_table = None
+
+    def on_pre_enter(self, *args):
+        print('---Pre-Entering ProjectAlunosScreen---')
         
-        #controi a tela
-        layout = MDBoxLayout(orientation='vertical')
+        # if self.initialized:
+        #     pass
+        
+        #TODO: alternativamente, testar se isso não é muito lento ou cria lixo na memória:
+        #todos os widgets serão descartados e recriados, mas isso pode simplificar algumas coisas
+        self.clear_widgets()
+        
+        #constroi a tela
+        layout = MDBoxLayout(orientation='vertical',
+                             md_bg_color='teal')
         self.add_widget(layout)
         
+        #cabeçalho
+        line0 = MDBoxLayout(orientation='horizontal',
+                            size_hint=(1, None),
+                            minimum_height=0)
+        layout.add_widget(line0)
+        
+        #botao para voltar pra tela principal
+        btn0 = MDIconButton(icon='arrow-left-bold',
+                            halign='left',
+                            icon_size='32sp',
+                            icon_color='white')
+        btn0.bind(on_release=lambda x: setattr(app.root, 'current', 'MainScreen'))
+        line0.add_widget(btn0)
+        
         #titulo
-        label = MDLabel(text="Project Screen", halign="center")
-        layout.add_widget(label)
-
-        #tabela
-        data_table = MDDataTable(
-            size_hint=(0.9, 0.6),
-            pos_hint={'center_x': 0.5, 'center_y': 0.5},
-            use_pagination=True,
-            column_data=[
-                ("No.", dp(30)),
-                ("Name", dp(30)),
-                ("Age", dp(30)),
-            ],
-            row_data=[
-                ("1", "John Doe", "29"),
-                ("2", "Jane Doe", "32"),
-                ("3", "Sam Smith", "45"),
-            ],
-        )
+        label = MDLabel(text=f'{app.curproject["nome"]} (Pessoas)',
+                        theme_text_color='Custom',
+                        font_style='H6',
+                        text_color='white',
+                        halign='center',
+                        valign='top',
+                        size_hint=(1, None)
+                        )
+        label.bind(texture_size=lambda instance, value: instance.setter('height')(instance, instance.texture_size[1] + 30))
+        line0.add_widget(label)
+        
+        #tabela #TODO: carregar se ja existir, ou criar se nao existir
+        alunos_df = app.curproject['alunos']
+        data_table = MDDataTable(size_hint=(1, .9), padding=5, elevation=2, 
+                                 pos_hint={'center_x': 0.5, 'center_y': 0.5},
+                                 use_pagination=False, rows_num=9999,
+                                 #colunas
+                                 column_data=[('nome', self.width),(f'', dp(0))],
+                                 #linhas
+                                 row_data=[(app.projetos[x]['nome'], f'') for x in range(len(app.projetos))]
+                                 )
+        data_table.ids.container.remove_widget(data_table.header)
+        data_table.sorted_on = "ID"
+        data_table.sorted_order = "ASC" #"DSC"
+        data_table.bind(on_row_press=self.on_row_press)
+        self.data_table = data_table
         layout.add_widget(data_table)
         
-        #barra inferior
+        #TODO: barra inferior
         
         #para nao adicionar de novo
         self.initialized = True
+    
+    def on_row_press(self, instance_table, instance_row):
+        print('on_row_press')
+        
+        global itable, irow
+        itable, irow = instance_table, instance_row
+        
+        #indentify which row it is #TODO: make it better if possible
+        #can't use the parent.children index because only some rows exist at any time
+        for i in range(len(app.projetos)):
+            if app.projetos[i]['nome']==irow.text:
+                print(f'found row index: {i}')
+                print(app.projetos[i]['nome'])
+                print(irow.text)
+        
+        #TODO: select the proper project table
+        #TODO: go to the project screen
+
+class ProjectEventosScreen(Screen):
+    initialized = False
+    data_table = None
+
+    def on_pre_enter(self, *args):
+        # if self.initialized:
+        #     pass
+        
+        #TODO: alternativamente, testar se isso não é muito lento ou cria lixo na memória:
+        #todos os widgets serão descartados e recriados, mas isso pode simplificar algumas coisas
+        self.clear_widgets()
+        
+        #constroi a tela
+        layout = MDBoxLayout(orientation='vertical',
+                             md_bg_color='teal')
+        self.add_widget(layout)
+        
+        #cabeçalho
+        line0 = MDBoxLayout(orientation='horizontal',
+                            size_hint=(1, None),
+                            minimum_height=0)
+        layout.add_widget(line0)
+        
+        #botao para voltar pra tela principal
+        btn0 = MDIconButton(icon='arrow-left-bold',
+                            halign='left',
+                            icon_size='32sp',
+                            icon_color='white')
+        btn0.bind(on_release=lambda x: setattr(app.root, 'current', 'MainScreen'))
+        line0.add_widget(btn0)
+        
+        #titulo
+        label = MDLabel(text=app.curproject['nome'],
+                        theme_text_color='Custom',
+                        font_style='H6',
+                        text_color='white',
+                        halign='center',
+                        valign='top',
+                        size_hint=(1, None)
+                        )
+        label.bind(texture_size=lambda instance, value: instance.setter('height')(instance, instance.texture_size[1] + 30))
+        line0.add_widget(label)
+        
+        #tabela #TODO: carregar se ja existir, ou criar se nao existir
+        alunos_df = app.curproject['alunos']
+        data_table = MDDataTable(size_hint=(1, .9), padding=5, elevation=2, 
+                                 pos_hint={'center_x': 0.5, 'center_y': 0.5},
+                                 use_pagination=False, rows_num=9999,
+                                 #colunas
+                                 column_data=[('nome', self.width),(f'', dp(0))],
+                                 #linhas
+                                 row_data=[(app.projetos[x]['nome'], f'') for x in range(len(app.projetos))]
+                                 )
+        data_table.ids.container.remove_widget(data_table.header)
+        data_table.sorted_on = "ID"
+        data_table.sorted_order = "ASC" #"DSC"
+        data_table.bind(on_row_press=self.on_row_press)
+        self.data_table = data_table
+        layout.add_widget(data_table)
+        
+        #TODO: barra inferior
+        
+        #para nao adicionar de novo
+        self.initialized = True
+    
+    def on_row_press(self, instance_table, instance_row):
+        print('on_row_press')
+        
+        global itable, irow
+        itable, irow = instance_table, instance_row
+        
+        #indentify which row it is #TODO: make it better if possible
+        #can't use the parent.children index because only some rows exist at any time
+        for i in range(len(app.projetos)):
+            if app.projetos[i]['nome']==irow.text:
+                print(f'found row index: {i}')
+                print(app.projetos[i]['nome'])
+                print(irow.text)
+        
+        #TODO: select the proper project table
+        #TODO: go to the project screen
 
 #%% tela de alunos/pessoas
 
@@ -159,19 +396,83 @@ class EventScreen(Screen):
 
 #%% app
 
-# Main App Class
 class ProjetoSocial(MDApp):
+    #stores the project list, each project is a dict with a name plus two pandas dataframes
+    projetos = []
+    curproject = None
+    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        # Config
+        #config
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Teal"
+        Metrics.fontscale = 0.8
         
+        #TODO: TEMP: using test data for now
+        for i in range(6):
+            self.create_test_project(index=i)
+    
     def build(self):
-        # Load the main KV layout string and return it
         return Builder.load_string(KV)
+    
+    def create_project(self, name):
+        if name==None:
+            pass #TODO: handle it
+        
+        #cria tabelas de aluno e de eventos, com os campos fixos
+        alunos_df = pd.DataFrame()
+        alunos_df.columns = ['id', 'nome', 'frequencia']
+        eventos_df = pd.DataFrame()
+        eventos_df.columns = ['id', 'ano']
+        
+        #constroi o projeto
+        projeto = {'nome':f'name', 'alunos':alunos_df, 'eventos':eventos_df}
+        
+        #adiciona
+        self.projetos.append(projeto)
+        self.curproject = self.projetos[-1]
+        pass
+    
+    #TODO: TEMP
+    def create_test_project(self, index=1):
+        #alunos
+        alunos_df = pd.DataFrame()
+        alunos_df['id'] = [x+1 for x in range(3)]
+        alunos_df['nome'] = [f'Aluno {x+1}' for x in range(3)]
+        alunos_df['frequencia'] = [[] for x in range(3)]
+        alunos_df.index = alunos_df.id
+        #eventos
+        eventos_df = pd.DataFrame()
+        eventos_df['id'] = [x+1 for x in range(7)]
+        eventos_df['year'] = [1234 for x in range(7)]
+        eventos_df.index = eventos_df.id
+        #constroi o projeto
+        projeto = {'nome':f'Projeto Exemplo {index}', 'alunos':alunos_df, 'eventos':eventos_df}
+        #adiciona
+        self.projetos.append(projeto)
+        self.curproject = self.projetos[-1]
+        pass
 
+#%% data handling
+
+
+
+#extrai as colunas do dataframe no formato que o kivymd usa
+def get_table_columns(df):
+    pass
+
+#extrai as linhas do dataframe no formato que o kivymd usa
+def get_table_rows(df):
+    pass
+
+#TODO: muito a fazer, salvar e carregar projetos, exportar/importar csv e json...
+
+#%% test data
+
+
+
+#%% run
 
 # Run the app
 if __name__ == '__main__':
